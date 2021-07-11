d0b3a9b5a4f3369706ea27da9cd34f9d9c3b35c6
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-07-11 21:15:28 +0300

    Update

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sefaekici/vue-excel-to-json.git main:gh-pages
+git remote add origin git@github.com:sefaekici/vue-excel-to-json.git
+git push -f origin master git@github.com:sefaekici/vue-excel-to-json.git main:gh-pages
 
 cd -
